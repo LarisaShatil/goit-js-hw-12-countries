36a0e0c1e2f9e6489dc0d13089bed54eec2923ef
LarisaShatil <larisa.shat@ukr.net>
2021-08-23 23:38:58 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
 import './sass/main.scss';
-import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
- 
-import fetchCountries from './js/fetchCountries.js';
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+import country from './templates/country.hbs';
+import list from './templates/list.hbs';
+import { alert, success, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
 
 
-success({
-  text: "Notice me, senpai!",
-});
+const refs = {
+  input: document.querySelector('#search'),
+  container: document.querySelector('div')
+}
+
+refs.input.addEventListener('input', debounce(fetchCountries, 500))
+
+function fetchCountries(e) {
+  e.preventDefault();
+  refs.container.innerHTML = '';
+
+  const name = refs.input.value;
+  // axios.get always brings Object
+  axios.get(`https://restcountries.eu/rest/v2/name/${name}`)
+    .then(result => renderResult(result.data))
+    .catch(err => (error({ text: "Type in a new query" }), console.log(err)))
+}
+
+function renderResult(arr) {
+
+  if (arr.length > 10) {
+alert({ text: "Could you specify the request?"});
+  }
+
+  if (arr.length === 1) {
+    buildMarkup(arr, country);
+    success({ text: "The country was found!"});
+      console.log(arr); 
+  }
+
+  else if (arr.length > 1 && arr.length <= 10) {
+    buildMarkup(arr, list);
+    console.log(arr);
+  }
+
+}
+
+function buildMarkup(arr, template) {
+  const markup = arr.map(el => template(el)).join('');
+  refs.container.insertAdjacentHTML('afterbegin', markup)
+}
+
+
+
+
+
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,16 +1,26 @@
 import axios from 'axios';
-import debounce from 'lodash.debounce'
-const refs = {
-  input: document.querySelector('#search')
-}
-export function fetchCountries(e) {
-  e.preventDefault();
-  const name = refs.input.value;
-  // axios.get always brings Object
-axios.get(`https://restcountries.eu/rest/v2/name/${name}`)
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
-}
 
-// export const searchQuery = 0;
-refs.input.addEventListener('input', debounce(fetchCountries, 500))
+
+
+// export function fetchCountries(e) {
+//   e.preventDefault();
+//   const name = refs.input.value;
+//   // axios.get always brings Object
+// axios.get(`https://restcountries.eu/rest/v2/name/${name}`)
+//     .then(result => console.log(result.data))
+//     .catch(err => console.log(err))
+// }
+
+// export default function fetchCountries(searchQuery) {
+//   return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(response => console.log(response.json()))
+// }
+
+// export function fetchCountries(e) {
+//   e.preventDefault();
+//   const name = refs.input.value;
+//   // axios.get always brings Object
+// axios.get(`https://restcountries.eu/rest/v2/name/${name}`)
+//     .then(result => renderResult(result.data))
+//     .catch(err => console.log(err))
+// }
+
