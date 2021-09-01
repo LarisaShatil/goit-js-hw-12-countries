1a89295239b8bff03aaf8ceecc2174f47ea1f56a
LarisaShatil <larisa.shat@ukr.net>
2021-09-01 03:03:44 +0300

    01.09.2021

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
 import './sass/main.scss';
 import axios from 'axios';
 import debounce from 'lodash.debounce';
+import getRefs from './js/refs';
 import country from './templates/country.hbs';
 import list from './templates/list.hbs';
 import { alert, success, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/BrightTheme.css';
 
+const BASE_URL = 'https://restcountries.eu/rest/v2';
+const refs = getRefs();
 
-const refs = {
-  input: document.querySelector('#search'),
-  container: document.querySelector('div')
+
+refs.input.addEventListener('input', debounce(fetchCountries, 1500))
+
+function fetchCountries(event) {
+  event.preventDefault();
+  refs.container.innerHTML = '';
+  const name = refs.input.value;
+  axios.get(`${BASE_URL}/name/${name}`)
+    .then(result => renderResult(result.data))
+    .catch(onError)
 }
 
-refs.input.addEventListener('input', debounce(fetchCountries, 500))
-
-function fetchCountries(e) {
-  e.preventDefault();
-  refs.container.innerHTML = '';
-
-  const name = refs.input.value;
-  // axios.get always brings Object
-  axios.get(`https://restcountries.eu/rest/v2/name/${name}`)
-    .then(result => renderResult(result.data))
-    .catch(err => (error({ text: "Type in a new query" }), console.log(err)))
+function onError(err) {
+  error({ text: "Type in a new query" });
+  console.log(err);
 }
 
 function renderResult(arr) {
-
   if (arr.length > 10) {
 alert({ text: "Could you specify the request?"});
   }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -14,13 +14,14 @@ import axios from 'axios';
 // export default function fetchCountries(searchQuery) {
 //   return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(response => console.log(response.json()))
 // }
+// const BASE_URL = 'https://restcountries.eu/rest/v2';
 
 // export function fetchCountries(e) {
 //   e.preventDefault();
+//   refs.container.innerHTML = '';
 //   const name = refs.input.value;
-//   // axios.get always brings Object
-// axios.get(`https://restcountries.eu/rest/v2/name/${name}`)
-//     .then(result => renderResult(result.data))
-//     .catch(err => console.log(err))
+//   return axios.get(`${BASE_URL}/name/${name}`).then(result => { return result });
+  
 // }
 
+// export default { fetchCountries };
